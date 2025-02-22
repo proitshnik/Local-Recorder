88d7d2fb0cfb9dc598820c686b85447dd1ab5793
qwers451 <mikhailzametaev@gmail.com>
2025-02-22 19:06:08 +0300

    Add fixes

--- playground/Zametaev/server/app.py
+++ playground/Zametaev/server/app.py
@@ -2,16 +2,32 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://mongo:27017/myDatabase"
+
+MONGO_HOST = "mongo"
+MONGO_PORT = 27017
+MONGO_DB = "myDatabase"
+
+app.config["MONGO_URI"] = f"mongodb://{MONGO_HOST}:{MONGO_PORT}/"
 mongo = PyMongo(app)
+
+db = mongo.db
+
 
 @app.route("/")
 def home():
-    # Вставка данных
-    mongo.db.users.insert_one({"name": "Алексей", "age": 30})
-    # Поиск данных
-    user = mongo.db.users.find_one({"name": "Алексей"})
-    return f"Привет, {user['name']}! Тебе {user['age']} лет."
+    try:
+        db.users.insert_one({"name": "Алексей", "age": 30})
+
+        user = db.users.find_one({"name": "Алексей"})
+
+        if user:
+            return f"Привет, {user['name']}! Тебе {user['age']} лет."
+        else:
+            return "Пользователь не найден.", 404
+
+    except Exception as e:
+        return f"Произошла ошибка: {str(e)}", 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
