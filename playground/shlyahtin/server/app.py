6588219695aa8f00f5bbba61e89303c9eb127406
mrekk-hub <maxim.shlyahtin@yandex.ru>
2025-02-23 12:43:32 +0300

    Applied suggested fixes

--- playground/shlyahtin/server/app.py
+++ playground/shlyahtin/server/app.py
@@ -5,7 +5,7 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 # Подключение к локальной MongoDB
-app.config["MONGO_URI"] = os.getenv("MONGO_URI", "mongodb://localhost:27017/testdb")
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 mongo = PyMongo(app)
 
 
@@ -17,18 +17,23 @@ def home():
 @app.route("/insert")
 def insert_data():
     """Тестовая вставка данных в коллекцию 'users'"""
-    users = mongo.db.users
-    user_id = users.insert_one({"name": "Test user", "age": 25}).inserted_id
-    return jsonify({"message": "User was added", "id": str(user_id)})
+    try:
+        users = mongo.db.users
+        user_id = users.insert_one({"name": "Test user", "age": 25}).inserted_id
+        return jsonify({"message": "User was added", "id": str(user_id)})
+    except Exception as e:
+        return f"Error while inserting data : {e}"
 
 
 @app.route("/get_users")
 def get_users():
     """Получение всех пользователей из коллекции 'users'"""
-    users = mongo.db.users.find()
-    result = [{"id": str(user["_id"]), "name": user["name"], "age": user["age"]} for user in users]
-    return jsonify(result)
-
+    try:
+        users = mongo.db.users.find()
+        result = [{"id": str(user["_id"]), "name": user["name"], "age": user["age"]} for user in users]
+        return jsonify(result)
+    except Exception as e:
+        return f"Error while extracting data : {e}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
