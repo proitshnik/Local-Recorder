a941092076c753bbea056e2f22b85eb48416cd63
Долотов Никита <112758213+Do1otov@users.noreply.github.com>
2025-02-24 15:59:16 +0300

    Add fixes

--- playground/dolotov/server/app.py
+++ playground/dolotov/server/app.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 import os
 
 app = Flask(__name__)
+
 client = MongoClient('mongodb://mongo:27017/')
 db = client.screen_recorder
 
@@ -10,13 +11,21 @@ db = client.screen_recorder
 def upload_file():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
+
     file = request.files['file']
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
-    file_path = os.path.join('/data', file.filename)
-    file.save(file_path)
-    db.recordings.insert_one({'filename': file.filename, 'path': file_path})
-    return jsonify({'success': True}), 200
+
+    try:
+        file_path = os.path.join('/data', file.filename)
+        file.save(file_path)
+
+        db.recordings.insert_one({'filename': file.filename, 'path': file_path})
+
+        return jsonify({'success': True}), 200
+    except Exception as e:
+        print(f"Ошибка при сохранении файла или записи в базу данных: {e}")
+        return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
