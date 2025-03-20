04579d9f991f4eb05be553cc73952d2b27f17c7c
Zazulya Ilya <112755498+Ascens1ve@users.noreply.github.com>
2025-03-20 17:31:41 +0300

    Fix duration
    Co-authored-by: Petr Bodunov <71722068+PetrBodunov@users.noreply.github.com>

--- client/media.js
+++ client/media.js
@@ -60,9 +60,17 @@ const getFormatDate = (date) => {
 
 const getDifferenceInTime = (date1, date2) => {
     const diff = Math.abs(date2.getTime() - date1.getTime()); // ms
-    const seconds = Math.round(diff / 1000);
-    const minutes = Math.round(seconds / 60);
-    const hours = Math.round(minutes / 60);
+    const totalSeconds = Math.floor(diff / 1000);
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    // Для удобного представления
+    const formattedHours = String(hours).padStart(2, '0');
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
     return `${hours}:${minutes}:${seconds}`;
 };
 
