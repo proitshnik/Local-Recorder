14200884ab9cfb589951b65381139d07039f4465
Do1otov <dolotok2@gmail.com>
2025-05-05 23:53:43 +0300

    Remove listener from background.js related to waitForNotificationSuppression() func

--- client/scripts/background.js
+++ client/scripts/background.js
@@ -135,23 +135,6 @@ chrome.runtime.onMessage.addListener(
 	}
 );
 
-chrome.runtime.onMessage.addListener(
-	function(message, sender, sendResponse) {
-		if (message.action === "stopMediaNotification") {
-			chrome.runtime.sendMessage({ action: 'suppressModalNotifyAT' }, (response) => {
-				if (chrome.runtime.lastError) {
-					console.error('Error send suppressModalNotifyAT', chrome.runtime.lastError.message);
-					logClientAction({ action: "Error send suppressModalNotifyAT", message: chrome.runtime.lastError.message});
-				}
-				else {
-					console.log('Response suppressModalNotifyAT', response);
-					logClientAction({ action: "Response suppressModalNotifyAT", response});
-				}
-			});
-		}
-	}
-);
-
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	if (message.type === 'screenCaptureStatus') {
 		logClientAction({ action: "listener screenCaptureStatus", active: message.active });
