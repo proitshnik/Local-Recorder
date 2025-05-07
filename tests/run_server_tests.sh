09faad980f8cd2e54ad330a0cd364b94e4f42cd8
proitshnik <mister.kalmak@yandex.ru>
2025-05-07 13:34:36 +0300

    Divide client and server scripts

--- tests/run_server_tests.sh
+++ tests/run_server_tests.sh
@@ -7,30 +7,19 @@ run_integration_test() {
     python3 integration_test.py
 }
 
-run_functional_test() {
-    echo "---"
-    echo "Запускаем функциональный тест"
-    echo "---"
-    pytest
-}
-
 echo "---"
 echo "Запускаем тестирование"
 
 if [ $# -eq 0 ]; then
     run_integration_test
-    run_functional_test
 else
     case "$1" in
         integration)
             run_integration_test
             ;;
-        functional)
-            run_functional_test
-            ;;
         *)
             echo "Неизвестный аргумент: $1"
-            echo "Правильный запуск: sh $0 [ |integration|functional]"
+            echo "Правильный запуск: sh $0 [ |integration]"
             ;;
     esac
 fi
